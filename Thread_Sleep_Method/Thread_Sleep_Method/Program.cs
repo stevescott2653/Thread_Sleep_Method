b7e763a10f4f2d133d55e29a78816810eb4d6d89
Steven Scott <stevescott2653@gmail.com>
2019-10-03 17:13:31 -0400

    Add files via upload

---
+++ Thread_Sleep_Method/Thread_Sleep_Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Sleep_Method
+{
+    using System.Threading;
+
+    public class MyThread
+    {
+        public void Thread1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(200);
+            }
+        }
+    }
+    public class TrheadExample
+    { 
+
+        static void Main(string[] args)
+            {
+                MyThread mt = new MyThread();
+                Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+                Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+                t1.Start();
+                t2.Start();
+            }
+    }
+}
+
